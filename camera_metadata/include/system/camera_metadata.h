8ffc9073e9efec1f177ded29de5ba81fd57e5193
anshuk <quic_anshuk@quicinc.com>
2024-03-18 16:32:04 +0530

    camera : update camera-metadata for HGY
    Change-Id: Ic07f853fbd1b296810c908198319751f8caa9e26

--- camera_metadata/include/system/camera_metadata.h
+++ camera_metadata/include/system/camera_metadata.h
@@ -12,6 +12,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ *
+ * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ *
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #ifndef SYSTEM_MEDIA_INCLUDE_ANDROID_CAMERA_METADATA_H
@@ -456,6 +462,12 @@ const char *get_camera_metadata_tag_name(uint32_t tag);
  */
 ANDROID_API
 int get_camera_metadata_tag_type(uint32_t tag);
+
+/**
+ * Parses metadata buffer to print tags names, section names and data.
+ */
+ANDROID_API
+void log_indented_camera_metadata(const camera_metadata_t* metadata);
 
 /**
  * Retrieve human-readable name of section the tag is in. Returns NULL if
