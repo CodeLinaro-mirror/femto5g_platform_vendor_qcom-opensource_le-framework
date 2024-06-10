0bf0b5cd047ae34ebf074eb2d873e6d591c943d8
Bo Fang <quic_bofang@quicinc.com>
2024-06-11 03:36:05 +0800

    Codec2: change ALOGI to ALOGD in createC2HandleGBM()
    That log is frame level not sequence level, should use ALOGD.
    
    Change-Id: Ic463ec6d8ce2bd34a6c63c008a6bf0c7b80eb9ee
    Signed-off-by: Bo Fang <quic_bofang@quicinc.com>

--- av/media/codec2/vndk_platform/C2AllocatorGBM.cpp
+++ av/media/codec2/vndk_platform/C2AllocatorGBM.cpp
@@ -284,7 +284,7 @@ static c2_status_t createC2HandleGBM(C2Handle *&handle, std::shared_ptr<BufferEn
             handleGBM->mInts.bo_hi = (uint32_t)(((uint64_t)bo >> 32) & 0xFFFFFFFF);
             handleGBM->mInts.need_free_ext_buf = 1;
 
-            ALOGI("GBM handle data: fd:%d meta_fd:%d ext_fd:%d width:%u height:%u format:0x%x "
+            ALOGD("GBM handle data: fd:%d meta_fd:%d ext_fd:%d width:%u height:%u format:0x%x "
                     "C2&GBM usage_lo:0x%x usage_hi:0x%x stride:%u slice_height:%u size:%u, bo:0x%" PRIx64,
                     handleGBM->data[0], handleGBM->data[1], handleGBM->data[2], handleGBM->data[3],
                     handleGBM->data[4], handleGBM->data[5], handleGBM->data[6], handleGBM->data[7],
